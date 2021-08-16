86bb5b0817b9e0c751d2d02f1144882be922a295
samlcy1997 <samlcy1997@gmail.com>
2021-08-16 22:17:24 +0800

    add DHT11, remove motor and LED light for v3 plant

--- Plant.ts
+++ Plant.ts
@@ -13,6 +13,116 @@ namespace Environment {
     //let numWindTurns = 0
     //let windMPH = 0
 
+    //-------DHT11---------------------------------------------------
+
+
+    export enum DHT11dataType {
+        //% block="temperature"
+        temperature,
+        //% block="humidity"
+        humidity
+    }
+    let temp = 0
+    let temp_pin = 0
+    let _temperature: number = -999.0
+    let _humidity: number = -999.0
+    let _readSuccessful: boolean = false
+    let _sensorresponding: boolean = false
+    let _firsttime: boolean = true
+
+    //% block="Get DHT11 at pin %dataPin|"
+    function dht11_queryData(dataPin: DigitalPin) {
+
+        if (_firsttime == true) {
+            _firsttime = false
+            dht11_queryData(dataPin)
+        }
+        //initialize
+        let startTime: number = 0
+        let endTime: number = 0
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        for (let index = 0; index < 40; index++) dataArray.push(false)
+        for (let index = 0; index < 5; index++) resultArray.push(0)
+        _humidity = -999.0
+        _temperature = -999.0
+        _readSuccessful = false
+        _sensorresponding = false
+        startTime = input.runningTimeMicros()
+
+        //request data
+        pins.digitalWritePin(dataPin, 0) //begin protocol
+        basic.pause(18)
+        pins.setPull(dataPin, PinPullMode.PullUp) //pull up data pin if needed
+        pins.digitalReadPin(dataPin)
+        control.waitMicros(40)
+        if (pins.digitalReadPin(dataPin) == 1) {
+            //if no respone,exit the loop to avoid Infinity loop
+            pins.setPull(dataPin, PinPullMode.PullNone) //release pull up
+        }
+        else {
+            pins.setPull(dataPin, PinPullMode.PullNone) //release pull up
+            while (pins.digitalReadPin(dataPin) == 0); //sensor response
+            while (pins.digitalReadPin(dataPin) == 1); //sensor response
+
+            //read data (5 bytes)
+            for (let index = 0; index < 40; index++) {
+                while (pins.digitalReadPin(dataPin) == 1);
+                while (pins.digitalReadPin(dataPin) == 0);
+                control.waitMicros(28)
+                //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
+                if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+            }
+
+            endTime = input.runningTimeMicros()
+
+            //convert byte number array to integer
+            for (let index = 0; index < 5; index++)
+                for (let index2 = 0; index2 < 8; index2++)
+                    if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+            //verify checksum
+            checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+            checksum = resultArray[4]
+            if (checksumTmp >= 512) checksumTmp -= 512
+            if (checksumTmp >= 256) checksumTmp -= 256
+            if (checksum == checksumTmp) _readSuccessful = true
+
+            //read data if checksum ok
+            if (_readSuccessful) {
+
+                //DHT11
+                _humidity = resultArray[0] + resultArray[1] / 100
+                _temperature = resultArray[2] + resultArray[3] / 100
+
+            }
+
+
+            //wait 1 sec after query 
+            basic.pause(1000)
+
+        }
+    }
+
+    //% block="DHT11 Read %dht11data| at pin %dht11pin|"
+    //% weight=90
+    export function readData(dht11data: DHT11dataType, dht11pin: DigitalPin): number {
+        // querydata
+        dht11_queryData(dht11pin)
+        //return temperature /humidity
+        if (dht11data == DHT11dataType.temperature && _readSuccessful)
+            return Math.round(_temperature)
+        else if (dht11data == DHT11dataType.humidity && _readSuccessful)
+            return Math.round(_humidity)
+        else return 0
+    }
+
+    //------DHT11-------------------------------------------------
+
+
+
     //--------BME280--------------------------------------------------
 
     // BME280 Addresses
@@ -165,15 +275,18 @@ namespace Environment {
 
     //------------------BH1750----------------------------------------------
 
-    let BH1750_I2C_ADDR= 35;
+    let BH1750_I2C_ADDR = 35;
     pins.i2cWriteNumber(BH1750_I2C_ADDR, 0x11, NumberFormat.UInt8BE); //turn on bh1750
 
     /**
     * get light intensity value from bh1750
     */
-     //% blockId="readBH1750" block="value of light intensity(lx) from BH1750"
+    //% blockId="readBH1750" block="value of light intensity(lx) from BH1750"
     export function getIntensity(): number {
-        let raw_value= Math.idiv(pins.i2cReadNumber(BH1750_I2C_ADDR, NumberFormat.UInt16BE) * 5, 6);
+       let raw_value= Math.idiv(pins.i2cReadNumber(BH1750_I2C_ADDR, NumberFormat.UInt16BE) * 5, 6);
+
+
+
 		return raw_value;
     }
 
@@ -204,7 +317,6 @@ namespace Environment {
     }
 
 
-   
 
 
 
@@ -225,15 +337,16 @@ namespace Environment {
 
 
 
-    //% blockId="smarthon_usb"
-    //% block="Set LED grow light to intensity %intensity at %pin"
-    //% intensity.min=0 intensity.max=1023
-    //% advanced=true
 
-    export function TurnUSB(intensity: number, pin: AnalogPin): void {
 
-        pins.analogWritePin(pin, intensity);
-    }
+
+
+
+
+
+
+
+
 
     //% blockId="smarthon_waterpump"
     //% block="Set Water pump to intensity %intensity at %pin"
@@ -256,30 +369,30 @@ namespace Environment {
     }
 
 
-    //% blockId="smarthon_motorfan"
-    //% block="Set Motor fan with speed %intensity at S1 %pin1 S2 %pin2"
-    //% intensity.min=-1023 intensity.max=1023
-    //% pin1.defl=AnalogPin.P14 pin2.defl=AnalogPin.P15
-    //% weight=45	
-    //%subcategory=more
 
-    export function TurnMotorCW(intensity: number, pin1: AnalogPin, pin2: AnalogPin): void {
-        if (intensity > 0) {
-            if (intensity > 0 && intensity < 400) { intensity = 400; }
-            pins.analogWritePin(pin1, intensity);
-            pins.analogWritePin(pin2, 0);
-        }
-        else if (intensity < 0) {
-            intensity = Math.abs(intensity);
-            if (intensity > 0 && intensity < 400) { intensity = 400; }
-            pins.analogWritePin(pin1, 0);
-            pins.analogWritePin(pin2, intensity);
-        }
-        else {
-            pins.analogWritePin(pin1, 0);
-            pins.analogWritePin(pin2, 0);
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //% blockId="smarthon_plantservo"
     //% block="Set Servo to degree %degree at %pin"
@@ -293,6 +406,7 @@ namespace Environment {
 
 
     //% block="value of BME280 %state"
+    //% weight=91
     export function octopus_BME280(state: BME280_state): number {
         switch (state) {
             case 0:

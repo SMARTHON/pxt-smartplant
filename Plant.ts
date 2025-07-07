4e0f2c7ed61e243b66feb8c562b10beb95a2499e
Larry-lai-smarthon <larry.lai.smarthon@gmail.com>
2025-07-07 16:21:31 +0800

    update DHT11 functions

--- Plant.ts
+++ Plant.ts
@@ -22,24 +22,20 @@ namespace Environment {
         //% block="humidity"
         humidity
     }
-    let temp = 0
-    let temp_pin = 0
     let _temperature: number = -999.0
     let _humidity: number = -999.0
     let _readSuccessful: boolean = false
     let _sensorresponding: boolean = false
-    let _firsttime: boolean = true
     let _last_successful_query_temperature: number = 0
     let _last_successful_query_humidity: number = 0
 
-
+    /**
+     * 
+     */
     //% block="Get DHT11 at pin %dataPin|"
-    function dht11_queryData(dataPin: DigitalPin) {
-
-        if (_firsttime == true) {
-            _firsttime = false
-            dht11_queryData(dataPin)
-        }
+    //% blockId="get_dht11_value"
+    //% weight=75
+    export function dht11_queryData(dataPin: DigitalPin) {
         //initialize
         let startTime: number = 0
         let endTime: number = 0
@@ -49,10 +45,10 @@ namespace Environment {
         let resultArray: number[] = []
         for (let index = 0; index < 40; index++) dataArray.push(false)
         for (let index = 0; index < 5; index++) resultArray.push(0)
-        _humidity = 0
-        _temperature = 0
+        _humidity = -999.0
+        _temperature = -999.0
         _readSuccessful = false
-
+    
 
         //request data
         pins.digitalWritePin(dataPin, 0) //begin protocol
@@ -72,19 +68,14 @@ namespace Environment {
 
             //read data (5 bytes)
             for (let index = 0; index < 40; index++) {
-                startTime = input.runningTimeMicros()
-                while (pins.digitalReadPin(dataPin) == 1) {
-                    endTime = input.runningTimeMicros()
-                    if ((endTime - startTime) > 150) { break; }
-                };
-                while (pins.digitalReadPin(dataPin) == 0) {
-                    endTime = input.runningTimeMicros()
-                    if ((endTime - startTime) > 150) { break; }
-                };
+                while (pins.digitalReadPin(dataPin) == 1);
+                while (pins.digitalReadPin(dataPin) == 0);
                 control.waitMicros(28)
-                //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
+                //if sensor still pull up data pin after 28 us it means 1, otherwise 0
                 if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
             }
+
+            endTime = input.runningTimeMicros()
 
             //convert byte number array to integer
             for (let index = 0; index < 5; index++)
@@ -151,19 +142,10 @@ namespace Environment {
 
     }
 
-    //% block="DHT11 Read %dht11data| at pin %dht11pin|"
+    //% block="DHT11 Read %dht11data"
     //% weight=79
-
-    export function readData(dht11data: DHT11dataType, dht11pin: DigitalPin): number {
+    export function readData(dht11data: DHT11dataType): number {
         // querydata
-        dht11_queryData(dht11pin)
-        //return temperature /humidity
-        // if (dht11data == DHT11dataType.temperature && _readSuccessful)
-        //     return Math.round(_temperature)
-        // else if (dht11data == DHT11dataType.humidity && _readSuccessful)
-        //     return Math.round(_humidity)
-        // else return 0
-
         if (dht11data == DHT11dataType.temperature) {
             return Math.round(_temperature)
         }
@@ -422,7 +404,7 @@ namespace Environment {
     //% weight=52
     export function ReadWaterTemp(watertemppin: AnalogPin): number {
         // let voltage = 0;
-        let watertemp = 0;
+        let watertemp = 0.0;
         let readvalue = pins.analogReadPin(watertemppin);
         readvalue -= 399;
         readvalue /= 15;
